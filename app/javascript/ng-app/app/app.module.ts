c4dd8ec4d9ed7dc3b5f209c9c03220faf7c51d47
SaharakBanana <mju5804101398@gmaejo.mju.ac.th>
2018-04-23 17:02:27 +0700

    test3

--- app/javascript/ng-app/app/app.module.ts
+++ app/javascript/ng-app/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { enableProdMode } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FirstComponent } from '../first/first.component';
+import { First2Component } from '../first2/first2.component';
 import { SecondComponent } from '../second/second.component';
 import { RouterModule, Routes } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
@@ -15,13 +16,16 @@ enableProdMode();
 const appRoutes: Routes = [
   { path: '', component: FirstComponent },
   { path: 'first', component: FirstComponent },
-  { path: 'second', component: SecondComponent }
+  { path: 'second', component: SecondComponent },
+  { path: 'firstxxx', component: First2Component }
+
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
+    First2Component,
     SecondComponent
   ],
   imports: [
---
+++ app/javascript/ng-app/first2/first2.component.ts
@@ -0,0 +1,101 @@
+import { Component } from '@angular/core';
+import templateString from './first2.html';
+
+@Component({
+  template: templateString
+})
+export class First2Component {
+  private editableText1: string;
+
+  private a01: number;
+  private a02: number;
+  private a03: number;
+  private b01: number;
+  private b02: number;
+  private b03: number;
+
+  private one: number;
+  private two: number;
+  private sum: number;
+
+  private c01
+  private c02
+  private c03
+  private d01
+  private d02
+  private d03
+
+  private select1: string;
+
+  private text1s;
+  private text2ss;
+
+  private txt1s;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.editableText1 = "Shoujo Shuumatsu Ryokou";
+
+    this.a01 = 1
+    this.a02 = 2
+    this.a03 = 3
+    this.b01 = 11
+    this.b02 = 12
+    this.b03 = 13
+
+    this.c01 = 1
+    this.c02 = 2
+    this.c03 = "sdnf"
+    this.d01 = 4
+    this.d02 = 6
+    this.d03 = 7
+
+    this.select1 = "มกราคม"
+
+    this.txt1s = ["Banana", "Papaya", "Panama","Dove"]
+
+    this.text1s = [{name: "Banana", id: 1}, {name: "Papaya", id: 2}, {name: "Panama", id: 3}]
+    this.text2ss = [["a01","a02","a03"],["b01","b02","b03"]]
+  }
+
+  button1Click() {
+    this.a01 = Math.random()*100;
+    this.a02 = Math.random()*100;
+    this.a03 = Math.random()*100;
+    this.b01 = Math.random()*100;
+    this.b02 = Math.random()*100;
+    this.b03 = Math.random()*100;
+  }
+
+  button1ClickTest() {
+    this.c01 = "dove"
+    this.c02 = "efdssd";
+    this.c03 = "dsfkhdsfkshdf";
+    this.d01 = "sdlfj;djfl;dsj";
+    this.d02 = "sdkflsf";
+    this.d03 = "sdlfjsfl;sdc";
+  }
+
+
+  select1Change(value) {
+    alert("เดือนที่คุณเลือกคือ :"+value)
+  }
+
+  input1Change(value) {
+    alert(value)
+  }
+
+  checkbox1Change(value) {
+    alert(value)
+  }
+
+  onetwo(one,two) {
+    
+  
+    return parseInt(one)+parseInt(two)==10;
+    
+   
+
+  }
+}
